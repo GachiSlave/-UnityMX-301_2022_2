201f021c5bbc1e3bb2b6ef0bce4c127b33ea8a14
Луговых Владислав Витальевич <vlad.lugovykh@mail.ru>
2022-06-01 14:13:04 +0500

    Ura pobeda 2

---
+++ udar1/Assets/udar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class udar : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            //Apply a force to this Rigidbody in direction of this GameObjects up axis
+            m_Rigidbody.AddForce(-transform.right * m_Thrust, ForceMode.Impulse);
+        }
+    }
+
+}
